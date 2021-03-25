dc7f7ad85bbe141ef0209d45db14e5a2c72b0ef1
ncbarker <71114211+ncbarker@users.noreply.github.com>
2021-03-24 20:08:27 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BarkerAssignment9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
